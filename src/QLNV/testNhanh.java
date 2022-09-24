1202f00196d187931519b0e938da22fbb19dff22
congthanhhh <luffymonkeydct@gmail.com>
2022-09-24 20:43:25 +0700

    C4

--- src/QLNV/testNhanh.java
+++ src/QLNV/testNhanh.java
@@ -11,5 +11,6 @@ package QLNV;
 public class testNhanh {
     public static void main(String[] args) {
         System.out.println("Hello branch alpha!");
+        System.err.println("Chao lan 2 nhanh alpha!");
     }
 }
